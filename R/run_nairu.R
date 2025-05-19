6229b7bad8cd41ca71b5ae34ae46ff109eab9222
Zac Gross <izacgross@gmail.com>
2025-05-19 15:44:36 +1000

    Update run_nairu.R

--- R/run_nairu.R
+++ R/run_nairu.R
@@ -160,9 +160,10 @@ est_data <- est_data %>%
   mutate(dummy3 = ifelse(date == "2020Q2",1,0))  %>%
   mutate(dummy4 = ifelse(date >= "2020Q2" & date <= "2020Q3", 1, 0))
 
-
+print(est_data)
+         
 write.csv(est_data,
-          file = file.path(out_dir, "EST_data.csv"),
+          file = file.path(out_dir, "est_data.csv"),
           row.names = FALSE)
 
 
