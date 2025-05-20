658b048a5b4b1f119aa1b5045dbf2d729243c729
Zac Gross <izacgross@gmail.com>
2025-05-20 22:00:53 +1000

    Update run_nairu.R

--- R/run_nairu.R
+++ R/run_nairu.R
@@ -180,7 +180,7 @@ data_list <- list(
 # Compile The Model
 compiled_model <- stan_model(file = file.path("stan", "NAIRU_baseline.stan"))
          
-sampled_model_baseline <- sampling(compiled_model, data = data_list, chains=4,iter = 200, control = list(max_treedepth = 15))
+sampled_model_baseline <- sampling(compiled_model, data = data_list, chains=10,iter = 10000, control = list(max_treedepth = 15))
 
 
 
