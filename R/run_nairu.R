2bdeb18a4db50391d49b80a9841667b5fdb761ad
Zac Gross <izacgross@gmail.com>
2025-06-17 11:19:55 +1000

    Update run_nairu.R

--- R/run_nairu.R
+++ R/run_nairu.R
@@ -42,101 +42,67 @@ vintage_dir <- file.path(out_dir, "vintages")
 dir.create(vintage_dir, showWarnings = FALSE, recursive = TRUE)
 
 
+
 #---------------------------------------------------------------------------------------------------------
-#Download Most Recent ABS and RBA Data
+# Download Most Recent ABS and RBA Data
 #---------------------------------------------------------------------------------------------------------
-#Import Data from ABS Website
+# Import Data from ABS Website
 abs_5206 <- read_abs(series_id = c("A2304402X", "A2302915V"))
 abs_6202 <- read_abs(series_id = c("A84423043C", "A84423047L"))
 abs_6457 <- read_abs(series_id = c("A2298279F"))
 abs_6345 <- read_abs(series_id = c("A2713849C"))
-abs_1364 <- read_abs(series_id = c("A2454521V", "A2454517C"))
 rba_g3 <- read_rba(series_id = c("GBONYLD")) 
-rba_g1 <- read_rba(series_id = c("GCPIOCPMTMQP","GCPITIQP","GCPINTIQP")) 
+#rba_g1 <- read_rba(series_id = c("GCPIOCPMTMQP","GCPITIQP","GCPINTIQP")) 
+rba_g1 <- read_abs(series_id = c("A3604510W","A2330530C","A2330575J")) 
 
-#---------------------------------------------------------------------------------------------------------		
-#CLEANUP ABS SPREADSHEETS
 #---------------------------------------------------------------------------------------------------------
-#5206.0 Australian National Accounts: National Income, Expenditure and Product
+# Cleanup ABS Spreadsheets
+#---------------------------------------------------------------------------------------------------------
+# 5206.0 Australian National Accounts
 R_5206 <- abs_5206 %>%
-  filter(series_id %in% c("A2304402X", "A2302915V")) %>% 
-  mutate(date = zoo::as.yearqtr(date)) %>% 
-  dplyr::select(date, series_id, value) 
-R_5206 <- distinct(R_5206,date,series_id, .keep_all= TRUE)
-R_5206 <- dcast(R_5206, date ~ series_id)
-R_5206 <- R_5206 %>%
-  mutate(NULC = A2302915V/A2304402X) %>%
-  mutate(DLNULC = 100*(log(NULC)-log(lag(NULC,1)))) %>%
-  select(date,DLNULC)
-
-
-R_6345 <- abs_6345 %>%
-  filter(series_id %in% c("A2713849C")) %>% 
-  mutate(date = zoo::as.yearqtr(date)) %>% 
-  dplyr::select(date, series_id, value) 
-R_6345 <- distinct(R_6345,date,series_id, .keep_all= TRUE)
-R_6345 <- dcast(R_6345, date ~ series_id)
-R_6345 <- R_6345 %>%
-  mutate(DLWPI = 100*(log(A2713849C)-log(lag(A2713849C,1)))) %>%
-  select(date,DLWPI)
-
-#6457.0 International Trade Price Indexes, Australia
-R_6457 <- abs_6457 %>%
-  filter(series_id %in% c("A2298279F")) %>%
+  filter(series_id %in% c("A2304402X", "A2302915V")) %>%
   mutate(date = zoo::as.yearqtr(date)) %>%
-  mutate(dl4pmcg = 100*(log(value)-log(lag(value,4)))) %>%
-  mutate(dlpmcg = 100*(log(value)-log(lag(value,1)))) %>%
-  dplyr::select(date, dl4pmcg)
+  dplyr::select(date, series_id, value) %>%
+  distinct(date, series_id, .keep_all = TRUE) %>%
+  dcast(date ~ series_id) %>%
+  mutate(NULC = A2302915V / A2304402X,
+         DLNULC = 100 * (log(NULC) - log(lag(NULC, 1)))) %>%
+  select(date, DLNULC)
 
-#6202.0 Labour Force, Australia - Monthly
+# 6345.0 WPI Data
+R_6345 <- abs_6345 %>%
+  filter(series_id == "A2713849C") %>%
+  mutate(date = zoo::as.yearqtr(date)) %>%
+  distinct(date, series_id, .keep_all = TRUE) %>%
+  dcast(date ~ series_id) %>%
+  mutate(DLWPI = 100 * (log(A2713849C) - log(lag(A2713849C, 1)))) %>%
+  select(date, DLWPI)
+
+# 6457.0 International Trade Price Indexes
+R_6457 <- abs_6457 %>%
+  filter(series_id == "A2298279F") %>%
+  mutate(date = zoo::as.yearqtr(date),
+         dl4pmcg = 100 * (log(value) - log(lag(value, 4)))) %>%
+  select(date, dl4pmcg)
+
+# 6202.0 Labour Force Data
 R_6202 <- abs_6202 %>%
   filter(series_id %in% c("A84423043C", "A84423047L")) %>%
-  select(date, series_id, value)
-R_6202 <- distinct(R_6202,date,series_id, .keep_all= TRUE)
-R_6202 <- dcast(R_6202, date ~ series_id)
-R_6202 <- R_6202 %>% group_by(date=floor_date(date, "quarter")) %>%
-  summarize(A84423043C=mean(A84423043C), A84423047L=mean(A84423047L)) %>%
-  mutate(date = zoo::as.yearqtr(date))
-R_6202 <- R_6202 %>%
-  mutate(LUR = 100*(1-A84423043C/A84423047L)) %>%
+  select(date, series_id, value) %>%
+  distinct(date, series_id, .keep_all = TRUE) %>%
+  dcast(date ~ series_id) %>%
+  group_by(date = floor_date(date, "quarter")) %>%
+  summarize(A84423043C = mean(A84423043C, na.rm = TRUE),
+            A84423047L = mean(A84423047L, na.rm = TRUE)) %>%
+  mutate(date = zoo::as.yearqtr(date),
+         LUR = 100 * (1 - A84423043C / A84423047L)) %>%
   select(date, LUR)
 
-#Import RBA Data#
-#Trimmed-Mean Inflation
-# series_ids <- c("GCPIOCPMTMQP","GCPITIQP","GCPINTIQP")
-# new_names <- c("DLPTM", "DLPT", "DLPNT")
-# 
-# # Create a named vector for renaming
-# rename_map <- setNames( series_ids, new_names)
-# 
-# # Process the data
-# R_g1 <- rba_g1 %>%
-#   filter(series_id %in% series_ids) %>%
-#   mutate(date = zoo::as.yearqtr(date)) %>%
-#   # Use mutate and rename to handle multiple series
-#   pivot_wider(names_from = series_id, values_from = value) %>%
-#   rename(!!!rename_map) %>%
-#   select(date, all_of(new_names))
-
 R_g1 <- rba_g1 %>%
-  filter(series_id %in% c("GCPIOCPMTMQP")) %>%
+  filter(series_id %in% c("A3604510W")) %>%
   mutate(date = zoo::as.yearqtr(date)) %>%
   rename(DLPTM = value) %>%
   select(date, DLPTM)
-
-R_g1T <- rba_g1 %>%
-  filter(series_id %in% c("GCPITIQP")) %>%
-  mutate(date = zoo::as.yearqtr(date)) %>%
-  rename(DLPT = value) %>%
-  select(date, DLPT)
-
-
-R_g1NT <- rba_g1 %>%
-  filter(series_id %in% c("GCPINTIQP")) %>%
-  mutate(date = zoo::as.yearqtr(date)) %>%
-  rename(DLPNT = value) %>%
-  select(date, DLPNT)
-
 
 
 #Bond-market inflation expectations
@@ -146,74 +112,57 @@ R_g3 <- rba_g3 %>%
   mutate(pie_bondq = ((1+value/100)^(1/4)-1)*100) %>%
   select(date, pie_bondq)
 
+
 #RBA inflation expectations
-myfile <- file.path("inputs", "PIE_RBAQ.CSV")
+myfile <- "/Users/igro0002/Downloads/NAIRU-master/PIE_RBAQ.CSV"
 pie_rbaq <- read_csv(myfile)
 pie_rbaq <- pie_rbaq %>%
   rename(date=OBS) %>%
   mutate(date = zoo::as.yearqtr(date))
 
 
-  latest_date_df1 <- max(R_5206$date)
-  latest_date_df2 <- max(R_g1$date)
+
+latest_date_df1 <- max(R_5206$date)
+latest_date_df2 <- max(R_g1$date)
+
+# Check if the latest date in df2 is one day less than in df1
+if (latest_date_df2 > latest_date_df1) {
+  # Get the most recent point from df2
+  recent_date <- R_g1 %>% filter(date == latest_date_df2) %>% select(date) 
+  recent_point <- R_5206 %>% filter(date == latest_date_df1) %>% select(DLNULC) 
   
-  # Check if the latest date in df2 is one day less than in df1
-  if (latest_date_df2 > latest_date_df1) {
-    # Get the most recent point from df2
-    recent_date <- R_g1 %>% filter(date == latest_date_df2) %>% select(date) 
-    recent_point <- R_5206 %>% filter(date == latest_date_df1) %>% select(DLNULC) 
+  combined_df <- merge(recent_date, recent_point, all = TRUE)
   
-    combined_df <- merge(recent_date, recent_point, all = TRUE)
-    
-    # Append the recent point to df2
-    R_5206 <- bind_rows(R_5206, combined_df)
-    
+  # Append the recent point to df2
+  R_5206 <- bind_rows(R_5206, combined_df)
+  
 }
 
 
-# ── Extend pie_rbaq forward to latest_date_df2 ────────────────────────────────
-latest_pie_date <- max(pie_rbaq$date)
 
-if (latest_date_df2 > latest_pie_date) {
-  
-  # Quarters we still need (as yearqtr objects)
-  new_dates <- seq(from = latest_pie_date + 0.25,  # next quarter
-                   to   = latest_date_df2,
-                   by   = 0.25)
-  
-  # Grab the last observed row (all columns) and duplicate for each new date
-  last_row  <- pie_rbaq %>% filter(date == latest_pie_date)
-  new_rows  <- purrr::map_dfr(new_dates, ~ last_row %>% mutate(date = .x))
-  
-  # Append and keep chronological order
-  pie_rbaq  <- bind_rows(pie_rbaq, new_rows) %>% arrange(date)
-}
-
-
-data_set <- list(R_6345, R_6457, R_6202, R_g1, R_g3, pie_rbaq, R_g1T,R_g1NT) %>%
+data_set <- list(R_5206, R_6457, R_6202, R_g1, pie_rbaq) %>%
   Reduce(function(dtf1,dtf2) left_join(dtf1,dtf2,by="date"), .)
 
-data_set$pie_bondq <- replace(data_set$pie_bondq,is.na(data_set$pie_bondq),2.5/4)
+#data_set$pie_bondq <- replace(data_set$pie_bondq,is.na(data_set$pie_bondq),2.5/4)
 
 
+data_set <- data_set %>%
+  filter(!is.na(date))
 
-
-#Pick Sample
+# Pick Sample
 est_data <- data_set %>%
-  filter(date>"1997q3")
-
-est_data <- est_data %>%
-  mutate(dummy1 = ifelse(date >= "2021Q3" & date <= "2023Q1", 1, 0)) %>%
-  mutate(dummy2 = ifelse(date >= "2022Q1" & date <= "2022Q4", 1, 0))  %>%
-  mutate(dummy3 = ifelse(date == "2020Q2",1,0))  %>%
-  mutate(dummy4 = ifelse(date >= "2020Q2" & date <= "2020Q3", 1, 0))
+  filter(date > "1997q3" & date < "2025q1") %>%
+  mutate(dummy1 = ifelse(date >= "2021Q3" & date <= "2023Q1", 1, 0),
+         dummy2 = ifelse(date >= "2022Q1" & date <= "2022Q4", 1, 0),
+         dummy3 = ifelse(date == "2020Q2", 1, 0),
+         dummy4 = ifelse(date == "2020Q3", 1, 0))
 
 print(out_dir)
          
 csv_path <- file.path(out_dir, "est_data.csv")
 readr::write_csv(est_data, csv_path)
 
-         myfile <- file.path(out_dir, "est_data.csv")
+myfile <- file.path(out_dir, "est_data.csv")
 test <- read_csv(myfile)
          
 # Subset Data for Stan
